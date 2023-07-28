b48fb65aec186217bbce2881b39827f603ef65cd
Modze Lewskil <135513706+modzelewskil970@users.noreply.github.com>
2023-07-28 10:52:39 +0000

    if2

---
+++ if2.js
@@ -0,0 +1,11 @@
+// check the age to determine the eligibility to vote
+let age = 15;
+let result;
+
+if (age >= 18) {
+      result = "You are eligible to vote.";
+} else {
+      result = "You are not eligible to vote yet.";
+}
+
+console.log(result);
